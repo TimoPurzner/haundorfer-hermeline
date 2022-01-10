e0ea8c50ba4e9ce2ddb8c05fa3aaa89c7c9eee1e
Timo Purzner <timo.purzner@codecamp-n.com>
2022-01-10 17:28:12 +0100

    anomation for hero logo

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = (props: Props) => {
             <img className={`${styles.logo} ${styles.light}`} src="/logo/logo_blue.svg" />
             <img className={`${styles.logo} ${styles.dark}`} src="/logo/logo_white.svg" />
             <div className={`${styles.action}`} onClick={scrollDown}>
-                <div className={styles.more}>Erfahre mehr</div>
+                <div className={styles.more}>Der Sportclub aus Haundorf</div>
                 <svg xmlns="http://www.w3.org/2000/svg" className={styles.arrow} fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" /></svg>
             </div>
         </div>
